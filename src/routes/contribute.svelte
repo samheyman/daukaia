1df73b32810e7065c923fa488c3f99a3b03ccf34
Sam Heyman <sam.heyman@gmail.com>
2022-09-13 13:51:24 +0200

    Add Donorbox payment widget

--- src/routes/contribute.svelte
+++ src/routes/contribute.svelte
@@ -17,29 +17,50 @@
 	<section class="methods">
 		<div class="container">
 			<div class="even-columns">
-				<div class="method centered">
+				<div class="method ">
 					<h1>{$t('contribute.generosity.header')}</h1>
+					<p>{$t('contribute.generosity.paragraph')}</p>
 					<div style="text-align:left">
 						<ul>
-							<li>
-								{$t('contribute.generosity.bank-card.description')}
-
-								<ul>
-									<li>{@html $t('contribute.generosity.bank-card.migranodearena')}</li>
-									<li>{@html $t('contribute.generosity.bank-card.donately')}</li>
-								</ul>
-							</li>
-							<li>{@html $t('contribute.generosity.direct-debit')}</li>
-
 							<li>{@html $t('contribute.generosity.transfer')}</li>
 							<li>{@html $t('contribute.generosity.facebook')}</li>
+							<li>
+								{@html $t('contribute.generosity.donorbox')}<br /><br />
+
+								<script src="https://donorbox.org/widget.js" paypalExpress="false"></script><iframe
+									src="https://donorbox.org/embed/daukaia"
+									name="donorbox"
+									allowpaymentrequest="allowpaymentrequest"
+									seamless="seamless"
+									frameborder="0"
+									scrolling="no"
+									height="900px"
+									width="100%"
+									style="max-width: 500px; min-width: 250px; max-height:none!important"
+								/>
+							</li>
+						</ul>
+						<p>
+							<!-- &#127466;&#127480; -->
+							{@html $t('contribute.generosity.spain-residents')}
+						</p>
+						<ul>
+							<li>
+								{$t('contribute.generosity.bank-card.description')}:
+								<br />
+								{@html $t('contribute.generosity.bank-card.migranodearena')}
+								{@html $t('contribute.generosity.bank-card.donately')}
+							</li>
+							<li>{@html $t('contribute.generosity.direct-debit')}</li>
 						</ul>
 					</div>
 				</div>
-				<div class="method centered">
+				<div class="method ">
 					<h1>{$t('contribute.time.header')}</h1>
+					<p />
 					<div style="text-align:left">
 						<ul><li>{$t('contribute.time.volunteering')}</li></ul>
+						<img class="" src="/images/children.png" />
 					</div>
 				</div>
 			</div>
@@ -66,5 +87,10 @@
 	ul {
 		margin-left: 2rem;
 		padding: 0;
+		list-style-type: none;
+		margin: 10px 0;
+	}
+	li {
+		padding: 10px 0;
 	}
 </style>
